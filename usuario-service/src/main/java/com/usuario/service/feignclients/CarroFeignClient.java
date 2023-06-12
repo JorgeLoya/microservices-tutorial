4eb6fc758d74ae6633bca74f3c8120ae6fd9b1fa
JorgeLoya <jorgeloyaic@gmail.com>
2023-06-11 23:44:04 -0600

    Removed unused url on feign clients

--- usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "carro-service", url = "http://localhost:8082", path = "/carro")
+@FeignClient(name = "carro-service", path = "/carro")
 public interface CarroFeignClient {
 
     @PostMapping()
